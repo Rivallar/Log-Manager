0520dbb0cf1b32a9a48b99717e7b2499696bb534
Rivallar <rrusanovich@gmail.com>
2025-07-10 17:15:00 +0300

    added config to access postgres

--- config.py
+++ config.py
@@ -13,6 +13,18 @@ class Settings(BaseSettings):
     AGENTLOG_PASSWORD: str
     AGENTLOG_HOST: str
 
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def DATABASE_URL(self):
+        """Returns DSN for async connection to postgress DB"""
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS} \
+                @{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
     model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
