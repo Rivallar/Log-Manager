096f2c54b08127748fb20af6a1266928dea0e09f
Rivallar <rrusanovich@gmail.com>
2025-07-11 14:26:58 +0300

    agentlog schema deleted, agentlog model new method

--- models/agentlog_model.py
+++ models/agentlog_model.py
@@ -22,3 +22,23 @@ class AgentLogModel(Base):
     command: Mapped[str]
     if_error: Mapped[bool]
     data: Mapped[str]
+
+    @staticmethod
+    def from_log_file(db_row: tuple):
+        """Transforms a row from logs database to an object"""
+        start_command_index = db_row[16].find("MML:")
+        command = db_row[16][start_command_index + len("MML:"):]
+        return AgentLogModel(
+            cmd_code=db_row[1],
+            user_type=db_row[2],
+            imsi=db_row[3],
+            msisdn=db_row[4],
+            log_time=datetime.strptime(db_row[6], '%Y-%m-%d %H:%M:%S'),
+            operator=db_row[7],
+            agent_type=db_row[8],
+            node_ip=db_row[10],
+            command=command,
+            if_error=bool(db_row[14]),
+            data=db_row[16],
+        )
+
