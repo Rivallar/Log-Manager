993c26a03cb1d0098376d074b17450a29b19cc50
Rivallar <rrusanovich@gmail.com>
2025-07-11 13:53:07 +0300

    repr method for readable object representation

--- database.py
+++ database.py
@@ -3,4 +3,9 @@ from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     """Parent class to all database models"""
-    pass
+
+    def __repr__(self):
+        cols = []
+        for col in self.__table__.columns.keys():
+            cols.append(f"{col}={getattr(self, col)}")
+        return f"<{self.__class__.__name__} {' '.join(cols)}>"
