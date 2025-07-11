eadb00b6da987bb12bb33eb7e34d15f144b0327b
Rivallar <rrusanovich@gmail.com>
2025-07-11 15:35:53 +0300

    introducing fastapi

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-import logging
-from log_management_scripts.collect_logs_and_cleanup import delete_all_files_in_log_folders, unzip_and_cleanup
+"""
+Main project file to run fastapi microservice
+"""
+import uvicorn
+from fastapi import FastAPI
 
-from config import settings
-from log_setups import log_setups, LogSetup
 
-logging.basicConfig(level=settings.LOGGING_LEVEL)
+app = FastAPI(docs_url="/docs")
 
-# delete_all_files_in_log_folders()
-for setup in log_setups:
-    unzip_and_cleanup(setup=setup)
+@app.get("/get_logs")
+async def get_logs() -> dict:
+    """Returns logs info according to a query"""
+    return { "message": "Here will be logs soon"}
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="0.0.0.0", port=8081, reload=True)
