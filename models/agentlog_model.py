d68c4f534ef0d66c78119bae109a54a6b82213cd
Rivallar <rrusanovich@gmail.com>
2025-07-15 15:08:10 +0300

    agentlog_model reworked with true_imsi, true_msisdn fields and indexes

--- models/agentlog_model.py
+++ models/agentlog_model.py
@@ -1,9 +1,31 @@
+import re
+
 from datetime import datetime
 from typing import Optional
 
+from sqlalchemy import Index, BigInteger
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database import Base
+
+
+def get_true_identities(imsi: Optional[str], msisdn: Optional[str]) -> tuple[int | None, int | None]:
+    """Extracts imsi and msisdn as integer numbers from given string values"""
+    true_imsi, true_msisdn = None, None
+
+    if imsi:
+        match_imsi = re.search(r'(25701\d{10})', imsi)
+        true_imsi = int(match_imsi.group(1)) if match_imsi else None
+        if not true_imsi:       # imsi field either blank or contains msisdn
+            match_msisdn = re.search(r'(375\d{9})', imsi)
+            true_msisdn = int(match_msisdn.group(1)) if match_msisdn else None
+    if not true_msisdn and msisdn:
+        match_msisdn = re.search(r'(375\d{9})', msisdn)
+        true_msisdn = int(match_msisdn.group(1)) if match_msisdn else None
+    return true_imsi, true_msisdn
+
+
+
 
 
 class AgentLogModel(Base):
@@ -21,6 +43,8 @@ class AgentLogModel(Base):
     node_ip: Mapped[str]
     command: Mapped[str]
     if_error: Mapped[bool]
+    true_imsi: Mapped[Optional[int]] = mapped_column(BigInteger)
+    true_msisdn: Mapped[Optional[int]] = mapped_column(BigInteger)
     data: Mapped[str]
 
     @staticmethod
@@ -28,6 +52,7 @@ class AgentLogModel(Base):
         """Transforms a row from logs database to an object"""
         start_command_index = db_row[16].find("MML:")
         command = db_row[16][start_command_index + len("MML:"):]
+        true_imsi, true_msisdn = get_true_identities(imsi=db_row[3], msisdn=db_row[4])
         return AgentLogModel(
             cmd_code=db_row[1],
             user_type=db_row[2],
@@ -39,6 +64,13 @@ class AgentLogModel(Base):
             node_ip=db_row[10],
             command=command,
             if_error=bool(db_row[14]),
+            true_imsi=true_imsi,
+            true_msisdn=true_msisdn,
             data=db_row[16],
         )
 
+    __table_args__ = (
+        Index("imsi_index", "true_imsi"),
+        Index("msisdn_index", "true_msisdn"),
+        Index("time_index", "log_time"),
+    )
