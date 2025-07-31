871ed49c9d684c09702310b1667d2e801ccbcc6e
Rivallar <rrusanovich@gmail.com>
2025-07-31 17:10:25 +0300

    true msisdn in soaplogs table

--- models/soaplog_model.py
+++ models/soaplog_model.py
@@ -1,7 +1,8 @@
+import re
 from datetime import datetime
 from typing import Optional
 
-from sqlalchemy import Index
+from sqlalchemy import Index, BigInteger
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database import Base
@@ -19,6 +20,7 @@ class SoapLogModel(Base):
     if_error: Mapped[bool]
     error_description: Mapped[Optional[str]]
     node_name: Mapped[str]
+    true_msisdn: Mapped[Optional[int]] = mapped_column(BigInteger)
 
     @staticmethod
     def get_request_body(full_request: str) -> str:
@@ -32,6 +34,7 @@ class SoapLogModel(Base):
     @staticmethod
     def from_log_file(db_row: tuple, node_name: str):
         """Transforms a row from logs database to an object"""
+        match_msisdn = re.search(r'(375\d{9})', db_row[5])
         return SoapLogModel(
             log_time=datetime.strptime(db_row[1], '%Y-%m-%d %H:%M:%S:%f'),
             cmd_code=db_row[3],
@@ -39,11 +42,11 @@ class SoapLogModel(Base):
             request=SoapLogModel.get_request_body(db_row[6]),
             if_error=bool(db_row[8]),
             error_description=db_row[9] if db_row[8] else None,
-            node_name=node_name
+            node_name=node_name,
+            true_msisdn = int(match_msisdn.group(1)) if match_msisdn else None
         )
-
-    
 
     __table_args__ = (
         Index("soaplog_time_index", "log_time"),
+        Index("soap_msisdn_index", "true_msisdn")
     )
